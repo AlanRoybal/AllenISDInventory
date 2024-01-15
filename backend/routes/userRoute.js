483a47d4603ae9132cf99180d54d9c4fdc562605
Alan Roybal <alanroybal05@gmail.com>
2024-01-14 19:34:48 -0600

    getting user data for profile page

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
-const { registerUser, loginUser, logout } = require("../controllers/userController");
+const { registerUser, loginUser, logout, getUser, loginStatus } = require("../controllers/userController");
 const router = express.Router();
+const protect = require("../middleWare/authMiddleware");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
+router.get("/getuser", protect, getUser);
+router.get("/loggedin", loginStatus);
 
 module.exports = router;
