ff41177551eb8075332185a389f5b2f7048177ef
Alan Roybal <alanroybal05@gmail.com>
2024-01-16 20:40:25 -0600

    only allows allenisd staff accounts

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema({
         unique: true,
         trim: true,
         match: [
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@allenisd.org$/,
             "Please enter a valid email"
         ]
     },
